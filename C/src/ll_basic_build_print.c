316b9e1913b418d31d18bf48b4d850b3000467a2
varun-vijayakumar <varun.vijayakumar.hpe@gmail.com>
2018-03-20 21:30:37 -0700

    Linked_lists in C
    Signed-off-by: varun-vijayakumar <varun.vijayakumar.hpe@gmail.com>

---
+++ C/src/ll_basic_build_print.c
@@ -0,0 +1,62 @@
+/*
+ * Given two lists, merge their nodes together to make one list,
+ * taking nodes alternately between the two lists.
+ * So ShuffleMerge() with {1, 2, 3} and {7, 13, 1}
+ * should yield {1, 7, 2, 13, 3, 1}.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LL1_LEN 2
+#define LL2_LEN 3
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+void build(struct node **head, int data) {
+	struct node *newNode = (struct node *) malloc (sizeof(struct node));
+	*newNode = (struct node){data, NULL};
+
+	if (!(*head)) {
+		*head = newNode;
+		return;
+	}
+
+    struct node *current = *head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+
+    current->next = newNode;
+}
+
+void print(struct node *head) {
+    if (!head) {
+        return;
+    }
+
+    while (head != NULL) {
+        printf("%d ",head->data);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+int main () {
+    struct node *left = NULL;
+    struct node *right = NULL;
+
+    int iter = 0;
+    for (iter = 0; iter < LL1_LEN; iter++) {
+        build(&left, iter+10);
+    }
+    print(left);
+
+    for (iter = 0; iter < LL2_LEN; iter++) {
+        build(&right, iter+20);
+    }
+	print(right);
+}
