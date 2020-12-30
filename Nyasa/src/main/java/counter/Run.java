280856a3d4010c6beb92536c5e65265c0319d5b9
varun-vijayakumar <varun.vijayakumar.hpe@gmail.com>
2020-12-30 13:47:22 -0800

    Working
    Signed-off-by: varun-vijayakumar <varun.vijayakumar.hpe@gmail.com>

--- Nyasa/src/main/java/counter/Run.java
+++ Nyasa/src/main/java/counter/Run.java
@@ -14,35 +14,28 @@ import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.TimeZone;
-import java.util.TreeMap;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class Run {
     private static final TreeMap<String, HashMap<String, Long>> urlCountMap = new TreeMap<>();
 
     public static void main(String[] args) {
+        urlHitCount(args);
+    }
+    public static void urlHitCount(String[] args) {
         Collections.synchronizedNavigableMap(urlCountMap);
         String fileName;
 
         if (args != null && args.length != 0) {
             fileName = args[0];
             try {
-                if (extractInfoFromFile(fileName)) {
-                    // urlHitCounter();
-                }
+                extractInfoFromFile(fileName);
             } catch (Exception e) {
                 System.out.println("Exception occured while reading file : " + e.getClass().getName());
             }
         }
     }
-
     private static boolean extractInfoFromFile(String fileName){
 
         try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
@@ -51,15 +44,46 @@ public class Run {
             e.printStackTrace();
             return false;
         }
+        // System.out.println(urlCountMap);
         for (Map.Entry<String, HashMap<String, Long>> entry : urlCountMap.entrySet()) {
             System.out.println(entry.getKey());
-            for (Map.Entry<String, Long> mapEntry : entry.getValue().entrySet()) {
-                System.out.println(mapEntry.getKey()+" "+mapEntry.getValue());
+            HashMap sortedMap = sortByValues(entry.getValue());
+            Set sortedKeys = sortedMap.entrySet();
+            Iterator iterator = sortedKeys.iterator();
+            while(iterator.hasNext()) {
+                Map.Entry sortedEntry = (Map.Entry)iterator.next();
+                System.out.println(sortedEntry.getKey()+" "+sortedEntry.getValue());
             }
+
         }
         return true;
     }
 
+    private static HashMap sortByValues(HashMap map) {
+        List list = new LinkedList(map.entrySet());
+        Comparator<Map.Entry<String, Long>> valueComparator = (e1, e2) -> {
+            Long v1 = e1.getValue();
+            Long v2 = e2.getValue();
+            if (v1 == v2) {
+                // if hit count is same, sort alphabetically.
+                String k1 = e1.getKey();
+                String k2 = e2.getKey();
+                return k1.compareTo(k2);
+            }
+            return v2.compareTo(v1);
+        };
+
+        List<Map.Entry<String, Long>> listOfEntries = new ArrayList<Map.Entry<String, Long>>(map.entrySet());
+
+        Collections.sort(listOfEntries, valueComparator);
+
+        LinkedHashMap<String, Long> sortedHashMap = new LinkedHashMap<>(listOfEntries.size());
+        for(Map.Entry<String, Long> entry : listOfEntries){
+            sortedHashMap.put(entry.getKey(), entry.getValue());
+        }
+
+        return sortedHashMap;
+    }
     private static void urlHitCounter(String urlData) {
 
         String dateInEpoch = urlData.substring(0, urlData.indexOf("|"));
